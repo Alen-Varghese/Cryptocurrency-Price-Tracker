bcfee8c33f66b8f7e3279c74a1bc2f9a8e543ebe
Alen <alenkondoor@gmail.com>
2025-03-09 17:19:26 +0530

    Eighth Commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import Header from '../components/Common/Header'
-import TabsComponent from '../components/Dashboard/Tabs'
+import React, {useState, useEffect} from 'react';
+import Header from '../components/Common/Header';
+import TabsComponent from '../components/Dashboard/Tabs';
+import axios from 'axios';
 
 function DashboardPage() {
+
+  const [coins,setCoins] = useState([]);
+
+  useEffect(() => {
+
+    axios
+    .get(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h&locale=en&precision=3'
+    )
+    .then((res) => {
+      console.log("RESPONSE>>>",res);
+      setCoins(res.data);
+    })
+    .catch((error) => {
+      console.log("ERROR>>>",error);
+    });
+  }, []);
+
   return (
     <div>
         <Header/>
-        <TabsComponent/>
+        <TabsComponent coins={coins}/>
     </div>
   )
 }
