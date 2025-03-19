3fdfc1b62b176a87e0c9ff9d05879bb6ee816dba
Alen <alenkondoor@gmail.com>
2025-03-19 10:36:59 +0530

    Tenth Commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,10 +2,21 @@ import React, {useState, useEffect} from 'react';
 import Header from '../components/Common/Header';
 import TabsComponent from '../components/Dashboard/Tabs';
 import axios from 'axios';
+import Search from '../components/Dashboard/Search';
 
 function DashboardPage() {
 
   const [coins,setCoins] = useState([]);
+  const[search, setSearch] = useState('');
+
+  const onSearchChange=(e)=>{
+    setSearch(e.target.value);
+  }
+
+  var filteredCoins = coins.filter((item) => 
+    item.name.toLowerCase().includes(search.toLowerCase()) || 
+    item.symbol.toLowerCase().includes(search.toLowerCase())
+  );
 
   useEffect(() => {
 
@@ -25,7 +36,8 @@ function DashboardPage() {
   return (
     <div>
         <Header/>
-        <TabsComponent coins={coins}/>
+        <Search search={search} onSearchChange={onSearchChange}/>
+        <TabsComponent coins={filteredCoins}/>
     </div>
   )
 }
